ad7dcf4d98920a3c2d769f757ebe049ef627d42b
zs <zs@lukai.com>
2025-05-05 22:09:16 +0900

    2025/5/5

--- crm/src/main/java/com/lukai/crm/web/filter/EncodingFilter.java
+++ crm/src/main/java/com/lukai/crm/web/filter/EncodingFilter.java
@@ -1,4 +1,24 @@
 package com.lukai.crm.web.filter;
 
-public class EncodingFilter {
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
+
+        System.out.println("文字エンコーディングフィルターに入る");
+        //过滤post请求日文参数乱码
+        //POSTリクエストの日本語パラメータ文字化けをフィルタリングする
+        req.setCharacterEncoding("utf-8");
+        //レスポンスの日本語文字化けをフィルタリングする
+        resp.setContentType("text/html;charset=utf-8");
+        //放行
+        //リクエストを通過させる
+        filterChain.doFilter(req, resp);
+    }
+
 }
