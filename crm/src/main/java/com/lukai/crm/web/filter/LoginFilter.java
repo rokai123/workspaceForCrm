0df46b947fbf7a2aa4174cebc6c996175f0e7cd7
zs <zs@lukai.com>
2025-05-08 18:12:09 +0900

    2025/5/8

--- crm/src/main/java/com/lukai/crm/web/filter/LoginFilter.java
+++ crm/src/main/java/com/lukai/crm/web/filter/LoginFilter.java
@@ -2,12 +2,16 @@ package com.lukai.crm.web.filter;
 
 import com.lukai.crm.settings.domain.User;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-
+@WebFilter(
+        filterName = "LoginFilter",
+        urlPatterns = {"*.do","*.jsp"}
+)
 public class LoginFilter implements Filter {
 
 
@@ -18,14 +22,30 @@ public class LoginFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
 
-        //如果user不为空说明登录过，放行
-        //userがnullでない場合、ログイン済みと判断し通過を許可する
-        if (user == null) {
+        //不应该被拦截的资源，自动放行
+        //フィルター除外リソースの自動解放
+        String path = request.getServletPath();
+        if("/settings/user/login.do".equals(path)||"/login.jsp".equals(path)) {
+            chain.doFilter(req, resp);
 
+
+        }else {
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("user");
+            //如果user不为空说明登录过，放行
+            //userがnullでない場合、ログイン済みと判断し通過を許可する
+            if (user != null) {
+                chain.doFilter(req, resp);
+
+            }else {
+//            response.sendRedirect("/crm/login.jsp");
+                response.sendRedirect(request.getContextPath()+"/login.jsp");
+
+            }
         }
+
+
     }
 
 }
--- crm/src/test/java/com/lukai/settings/test/Test1.java
+++ crm/src/test/java/com/lukai/settings/test/Test1.java
@@ -28,6 +28,5 @@ public class Test1 {
 //        System.out.println(pwd);
 
 
-
     }
 }
